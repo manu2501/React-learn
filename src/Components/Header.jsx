4ec22ca9aff9062ae707a8548e0923e39ea4c4bc
manu2501 <manoj2rox@gmail.com>
2020-07-14 08:30:57 +0530

    initial commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Header.css'
+
+function Header() {
+    return (
+        <div className="Header-tag">
+            <h1>Header</h1>
+        </div>
+    )
+}
+
+export default Header
